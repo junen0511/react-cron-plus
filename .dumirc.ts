bb6c07de4a29e61f1d16607a0174a08cad138f03
TanFei <tanfei_68@163.com>
2023-06-15 17:55:07 +0800

    docs: site doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'dumi';
+
+const repo = 'react-cron-plus'; // 项目名
+
 export default defineConfig({
   outputPath: 'docs-dist',
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
@@ -8,4 +11,9 @@ export default defineConfig({
     rtl: true,
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
+  history: {
+    type: 'hash', // 打包后
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/react-cron-plus/' : '/', // 静态资源地址 ./其实也可以
+  hash: true, // 这块是静态资源会标记hash 并不是路由中的hash 不要弄混了
 });
