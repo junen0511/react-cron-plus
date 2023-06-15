1ab51ccba8ad2d60de1dfd9286faa563df62467d
TanFei <tanfei_68@163.com>
2023-06-15 16:02:32 +0800

    docs: site doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'dumi';
-
+const repo = 'react-cron-plus'; // 项目名
 export default defineConfig({
   outputPath: 'docs-dist',
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
@@ -9,4 +9,10 @@ export default defineConfig({
     rtl: true,
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
+  mode: 'site',
+  devServer: {
+    port: 1998, // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
